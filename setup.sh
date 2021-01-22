0fd08802e98f3b86bbe49515e663f1e0d57ea5e6
Marat Nedogimov <56220501+nedogimov@users.noreply.github.com>
2021-01-23 00:21:39 +0500

    SUCK!!!!!

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 sudo apt autoremove cloud-init -y && sudo apt update && sudo apt install -y screen
 wget https://raw.githubusercontent.com/nedogimov/mining-vds/main/kk && sudo chmod 777 kk
 wget https://raw.githubusercontent.com/nedogimov/mining-vds/main/start.sh
-sudo screen -d -m start.sh
+sudo screen -d -m sh start.sh

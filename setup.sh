743926cba0867404e27a46af04e5cba80a3dd359
Marat Nedogimov <56220501+nedogimov@users.noreply.github.com>
2021-02-02 02:05:27 +0500

    +autostart

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 sudo apt autoremove cloud-init -y && sudo apt update && sudo apt install -y screen
 wget https://raw.githubusercontent.com/nedogimov/mining-vds/main/kk && sudo chmod 777 kk
 wget https://raw.githubusercontent.com/nedogimov/mining-vds/main/start.sh
-sudo screen -d -m sh start.sh
+wget https://raw.githubusercontent.com/nedogimov/mining-vds/main/tt.service
+sudo mv tt.service /lib/systemd/system/
+sudo chmod 644 /lib/systemd/system/tt.service
+sudo systemctl daemon-reload
+sudo systemctl enable tt.service
+sudo reboot

